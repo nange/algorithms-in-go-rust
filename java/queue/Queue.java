7ad151cae8aa604657f38bbe7addff51e45f9b51
nange <463499510@qq.com>
2015-01-20 23:52:01 +0800

    update java queue

---
+++ java/queue/Queue.java
@@ -0,0 +1,41 @@
+package queue;
+
+public class Queue<T> {
+	
+	private T[] dataStore;
+	private int theSize;
+	
+	public Queue() {
+		
+	}
+	
+	public void enqueue(T element) {
+		
+	}
+	
+	public T dequeue() {
+		return null;
+	}
+	
+	public int size() {
+		return 0;
+	}
+	
+	public boolean isEmpty() {
+		return false;
+	}
+	
+	public T font() {
+		return null;
+	}
+	
+	public T end() {
+		return null;
+	}
+	
+	public void clear() {
+		
+	}
+	
+
+}
