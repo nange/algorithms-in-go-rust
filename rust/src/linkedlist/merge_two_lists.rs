3d89a9bf2faa2425d67b658a489bdfce489c252d
nange <cctonange@gmail.com>
2023-07-11 15:45:21 +0800

    update

--- rust/src/linkedlist/merge_two_lists.rs
+++ rust/src/linkedlist/merge_two_lists.rs
@@ -11,31 +11,34 @@ pub fn merge_two_lists(
     list1: Option<Box<ListNode>>,
     list2: Option<Box<ListNode>>,
 ) -> Option<Box<ListNode>> {
-    let mut dummy_head = Some(Box::new(ListNode { val: 0, next: None }));
-    let mut tail = &mut dummy_head;
+    let mut dummy = Some(Box::new(ListNode { val: 0, next: None }));
+    let mut p = &mut dummy;
 
-    let mut l1 = list1;
-    let mut l2 = list2;
-    while l1.is_some() && l2.is_some() {
-        if l1.as_ref().unwrap().val < l2.as_ref().unwrap().val {
-            let next = l1.as_mut().unwrap().next.take();
-            tail.as_mut().unwrap().next = l1;
-            l1 = next;
+    let mut p1 = list1;
+    let mut p2 = list2;
+
+    while p1.is_some() && p2.is_some() {
+        if p1.as_ref().unwrap().val <= p2.as_ref().unwrap().val {
+            let next = p1.as_mut().unwrap().next.take();
+            p.as_mut().unwrap().next = p1;
+            p1 = next;
         } else {
-            let next = l2.as_mut().unwrap().next.take();
-            tail.as_mut().unwrap().next = l2;
-            l2 = next;
+            let next = p2.as_mut().unwrap().next.take();
+            p.as_mut().unwrap().next = p2;
+            p2 = next;
         }
-        tail = &mut tail.as_mut().unwrap().next;
+
+        p = &mut p.as_mut().unwrap().next;
     }
 
-    if l1.is_some() {
-        tail.as_mut().unwrap().next = l1;
-    } else {
-        tail.as_mut().unwrap().next = l2;
+    if p1.is_some() {
+        p.as_mut().unwrap().next = p1;
+    }
+    if p2.is_some() {
+        p.as_mut().unwrap().next = p2;
     }
 
-    dummy_head.unwrap().next
+    dummy.unwrap().next
 }
 
 #[cfg(test)]
@@ -53,14 +56,20 @@ mod tests {
         assert_eq!(ret2, Some(Box::new(ListNode { val: 1, next: None })));
 
         let ret3 = merge_two_lists(
-            Some(Box::new(ListNode { val: 1, next: None })),
+            Some(Box::new(ListNode {
+                val: 1,
+                next: Some(Box::new(ListNode { val: 3, next: None })),
+            })),
             Some(Box::new(ListNode { val: 2, next: None })),
         );
         assert_eq!(
             ret3,
             Some(Box::new(ListNode {
                 val: 1,
-                next: Some(Box::new(ListNode { val: 2, next: None }))
+                next: Some(Box::new(ListNode {
+                    val: 2,
+                    next: Some(Box::new(ListNode { val: 3, next: None }))
+                }))
             }))
         )
     }
